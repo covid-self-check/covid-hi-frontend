09b8892acd3d5efe9e6abef598cc33e43cfe238e
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-08-01 04:05:09 +0700

    feat: add ComCovid theme color to project

---
+++ styles/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3E54D3",
+      light: "#4F80E2",
+    },
+    secondary: {
+      main: "#15CDCA",
+      light: "#4FE0B6",
+    },
+  },
+});
+
+export default theme;
