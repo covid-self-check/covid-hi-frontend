c38ff0a09526d60a8ae2ec88e3dd749392658910
ktns <pom-2551@hotmail.com>
2021-08-01 03:02:10 +0700

    feat: added registration form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import styles from "../styles/Home.module.css";
+import RegistrationForm from "../components/RegistrationForm";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <RegistrationForm />
+      </div>
+    </>
+  );
 }
