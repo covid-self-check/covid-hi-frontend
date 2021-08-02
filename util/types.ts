68788e878f21a76eb0aeb7e897fe6f8ea6b4cfe5
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-08-02 09:51:17 +0700

    feat: add type format function and fix vaccination bug

--- util/types.ts
+++ util/types.ts
@@ -5,9 +5,9 @@ export type registerData = {
   station: string
   age: number
   gender: string
-  weight: string
+  weight: number
   height: number
-  congenitalDisease: string
+  congenitalDisease?: string
   address: string
   hasHelper: boolean
   digitalLiteracy: boolean
@@ -27,4 +27,79 @@ export type updateData = {
   pulseRate: number //pulse rate
   checklist: string[] // a list of covid symptoms
   exhaustionLevel: number //ระดับความรู้สึกว่าเหนื่อยไหม
+}
+
+export type registerFormData = {
+  firstName: string
+  lastName: string
+  personalID: string
+  age: string
+  weight: string
+  height: string
+  gender: string
+  address: string
+  addressInfo: {
+    province: string
+    district: string
+    subdistrict: string
+    postalCode: string
+  }
+  congenitalDisease: string
+  hasHelper: boolean
+  digitalLiteracy: boolean
+  contactInfo: {
+    phoneNumber: string
+    closeContactsPhoneNumber: string
+  }
+  lineID: string
+  vaccination: string
+  vaccinationDates: {
+    firstDoseName: string
+    firstDoseDate: string
+    secondDoseName: string
+    secondDoseDate: string
+  }
+}
+
+export const convertFormDataToAPIData: (data: registerFormData) => registerData = (data) => {
+  const {
+    firstName,
+    lastName,
+    personalID,
+    age,
+    weight,
+    height,
+    gender,
+    address,
+    addressInfo,
+    congenitalDisease,
+    hasHelper,
+    contactInfo,
+    lineID,
+    digitalLiteracy,
+    vaccinationDates,
+  } = data
+  const convertedData: registerData = {
+    firstName,
+    lastName,
+    personalID,
+    station: 'test', // no form yet
+    age: parseInt(age),
+    gender,
+    weight: parseInt(weight),
+    height: parseInt(height),
+    congenitalDisease,
+    address: `${address} ${addressInfo.subdistrict} ${addressInfo.district} ${addressInfo.province} ${addressInfo.postalCode}`,
+    hasHelper,
+    digitalLiteracy,
+    personalPhoneNo: contactInfo.phoneNumber,
+    personalLineID: lineID,
+    emergencyPhoneNo: '', // no form yet
+    closestUnriskPersonPhoneNo: contactInfo.closeContactsPhoneNumber,
+    dose1Name: vaccinationDates.firstDoseName,
+    dose1Date: vaccinationDates.firstDoseDate,
+    dose2Name: vaccinationDates.secondDoseName,
+    dose2Date: vaccinationDates.secondDoseDate,
+  }
+  return convertedData
 }
