8cd97d6c64fe700b5f9a8e522dcb4cd39fba14b2
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-08-04 02:02:14 +0700

    fix: navbar icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Icon, SvgIcon, Toolbar, Typography } from '@material-ui/core'
 import Link from 'next/link'
+import logo from '../public/logo.svg'
 import { useRouter } from 'next/dist/client/router'
 import { makeStyles } from '@material-ui/styles'
 import theme from '../styles/theme'
+import Image from 'next/image'
 
 const useStyles = makeStyles({
   link: {
@@ -26,7 +28,7 @@ const useStyles = makeStyles({
 export const Navbar = () => {
   return (
     <Toolbar sx={{ px: 3, py: 5 }}>
-      <img src="./logo.svg" width="20%" alt="logo" />
+      <Image src={logo} alt="Covid Self Check" width="60px" height="60px" />
       <Typography
         variant="h4"
         component="h4"
