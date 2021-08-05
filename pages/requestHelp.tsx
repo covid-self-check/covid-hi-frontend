8e63054c2a934181c22669e806e505082196fa51
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-08-06 02:25:08 +0700

    feat: hide navbar for requestHelp page

--- pages/requestHelp.tsx
+++ pages/requestHelp.tsx
@@ -1,5 +1,28 @@
+import { makeStyles } from '@material-ui/styles'
 import PhoneNumberForm from '../components/PhoneNumberForm'
 
+const useStyles = makeStyles(
+  {
+    container: {
+      marginTop: '16px',
+      marginBottom: '20px',
+      minHeight: '100vh',
+      padding: '0 0.5rem',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      height: 'auto',
+      paddingBottom: '30px',
+    },
+  },
+  { index: 1 },
+)
+
 export default function RequestHelp() {
-  return <PhoneNumberForm />
+  const styles = useStyles()
+  return (
+    <div className={styles.container}>
+      <PhoneNumberForm />
+    </div>
+  )
 }
